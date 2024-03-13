dbe29f8e566b9bc40f5d49711094ff603b4466f5
xuanson1 <hustle50k@gmail.com>
2024-03-13 18:54:27 +0700

    TH-SM

---
+++ my.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+// function showMessage(){
+//     document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+// }
+// function showMessage(){
+//     document.write('Xin chào!');
+// }
+// function showMessage(){
+//     console.log('Xin chào!');
+// }
